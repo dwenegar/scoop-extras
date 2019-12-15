fd408e1bcfe11c0907f763740f3d208a342b25b6
Simone Livieri <simone.livieri@gmail.com>
2019-12-15 18:14:39 +0900

    luarocks: install config

---
+++ scripts/luarocks-config.lua
@@ -0,0 +1,7 @@
+lua_interpreter = [[$scoopdir\apps\lua\current\bin\lua53.exe]]
+variables = {
+   LUA_BINDIR = [[$scoopdir\apps\lua\current\bin]],
+   LUA_DIR = [[$scoopdir\apps\lua\current]],
+   LUA_INCDIR = [[$scoopdir\apps\lua\current\include\lua\5.3]],
+   LUA_LIBDIR = [[$scoopdir\apps\lua\current\lib]]
+}
